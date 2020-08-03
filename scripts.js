d6028e2609f921ca67f57eee9511e55f620fccd9
moshiurshimul <hello@moshiurshimul.com>
2020-08-04 03:04:54 +0600

    final version

--- scripts.js
+++ scripts.js
@@ -1,15 +1,14 @@
 // Hiding Alert Message
     document.getElementById('pin-match').style.display = 'none';
     document.getElementById('pin-not-match').style.display = 'none';
-// Generating Pin
 
+// Generating 4 Digit Rendom Pin
 document.getElementById('pin-btn').addEventListener('click', function() {
     let fourDigitPin = Math.floor(1000 + Math.random() * 9000);
     document.getElementById('auto-pin').value = fourDigitPin;
 });
 
 // Number Input Function 
-
 let inputNumberBox = document.getElementById('input-number');
 
 function inputNumber(number){
@@ -17,36 +16,48 @@ function inputNumber(number){
 }
 
 // C Button Function
-
 function clearAll() {
     inputNumberBox.value = '';
 }
 
 
 // Back Button Function
-
 function backBtn() {
     currentNumbers = inputNumberBox.value;
     inputNumberBox.value = currentNumbers.slice(0, -1);
 }
 
 // Pin Matching Function 
-
 document.getElementById('submitBtn').addEventListener('click', function() {
-    let generatedPin = document.getElementById('auto-pin').value
+    let generatedPin = document.getElementById('auto-pin').value;
 
     if (generatedPin == "") {
         alert('Please Generate a Pin First.');
-    } 
+    }
     
     if (inputNumberBox.value.length != 4 ) {
         alert('Pin must be 4 Digit');
     }
+
     if (generatedPin == inputNumberBox.value) {
         document.getElementById('pin-match').style.display = 'block';
+        document.getElementById('pin-not-match').style.display = 'none';
     }
+
     if (generatedPin != inputNumberBox.value) {
         document.getElementById('pin-not-match').style.display = 'block';
+        document.getElementById('pin-match').style.display = 'none';
+        
+        
+        // Disableing Submit Button
+        let chanceLeft = document.getElementById('chance-left').innerText;
+        let chanceLeftNumber = parseInt(chanceLeft);
+        
+        let currentChance = chanceLeftNumber - 1;
+        document.getElementById('chance-left').innerText = currentChance;
+        if(currentChance == 0) {
+            document.getElementById('submitBtn').disabled = 'true';
+        }
     }
-})
+});
 
