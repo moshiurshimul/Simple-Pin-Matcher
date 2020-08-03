fc1a1c02d347fcbb4910cb3c0e43daf5a8e2321f
moshiurshimul <hello@moshiurshimul.com>
2020-08-04 01:13:29 +0600

    number added

--- scripts.js
+++ scripts.js
@@ -7,4 +7,24 @@ document.getElementById('pin-btn').addEventListener('click', function() {
     document.getElementById('auto-pin').value = fourDigitPin;
 });
 
-// 
+// Number Input Function 
+
+let inputNumberBox = document.getElementById('input-number');
+
+function inputNumber(number){
+    inputNumberBox.value += number;
+}
+
+// C Button Function
+
+function clearAll() {
+    inputNumberBox.value = '';
+}
+
+// Back Button Function
+
+function backBtn() {
+    currentNumbers = inputNumberBox.value;
+    inputNumberBox.value = currentNumbers.slice(0, -1);
+} 
+
